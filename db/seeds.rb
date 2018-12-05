4a859cb8bc82646507d17555415e1e312a735599
yli0607x <github email address>
2018-12-05 10:17:57 -0500

    add seed & schema to gitignore

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name:"Sydney")
+
+Day.create(name:"Monday", user_id:1)
+Day.create(name:"Tuesday", user_id:1)
+Day.create(name:"Wednesday", user_id:1)
+Day.create(name:"Thursday", user_id:1)
+Day.create(name:"Friday", user_id:1)
+Day.create(name:"Saturday", user_id:1)
+Day.create(name:"Sunday", user_id:1)
+
+Food.create(name:"Mac n Cheese Lunch", day_id:1, calories:500)
+Food.create(name:"Mac n Cheese Dinner", day_id:1, calories:500)
+Food.create(name:"almond butter toast", day_id:2, calories:500)
+Food.create(name:"poke", day_id:2, calories:500)
+Food.create(name:"salad", day_id:3, calories:500)
+Food.create(name:"salad", day_id:4, calories:500)
+Food.create(name:"salad", day_id:5, calories:500)
+Food.create(name:"salad", day_id:6, calories:500)
+Food.create(name:"nothing", day_id:7, calories:500)
